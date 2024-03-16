9fadedc1f81603cab75d0a149cd52abb5aa23a17
andrew.deng <kobe73er@gmail.com>
2024-03-16 19:42:08 +0800

    fix format violation

--- src/main/java/org/interview/resource/BaseResource.java
+++ src/main/java/org/interview/resource/BaseResource.java
@@ -5,7 +5,6 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -44,5 +43,4 @@ public class BaseResource {
     response.setKubernetes(environmentService.isRunningInKubernetes());
     return response;
   }
-
 }
